d730badf9371c8aae8ce4a21590107afaef05db2
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-09-15 12:47:55 -0300

    melhorando estilizacao

--- src/componentes/header/index.tsx
+++ src/componentes/header/index.tsx
@@ -4,7 +4,9 @@ import styles from './header.module.css';
 function Header() {
   return (
     <header className={ styles.headerContainer }>
-      <img src={ logo } alt="logo Trybe News" />
+      <div className={ styles.logoContainer }>
+        <img src={ logo } alt="logo Trybe News" />
+      </div>
       <div className={ styles.titleContainer }>
         <h1> TRYBE NEWS</h1>
       </div>
